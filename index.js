dbd54a259002abc3972e3b8e2828ff80f0fedfcb
Arthur C. A <159676770+Arthur-C-A@users.noreply.github.com>
2024-03-13 20:47:42 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+
+let chars = document.querySelectorAll(".drum");
+let songs = ["crash.mp3", "kick-bass.mp3", "snare.mp3", "tom-1.mp3", "tom-2.mp3","tom-3.mp3", "tom-4.mp3"];
+
+for(let i = 0 ; i < chars.length ; i++){
+    chars[i].addEventListener("click", function () {
+        let innerHTML = this.innerHTML;
+        songForKeyBoard(innerHTML);
+    }); 
+
+}
+
+document.addEventListener('keydown', function(event){
+    songForKeyBoard(event.key);
+});
+
+function songForKeyBoard(key){
+        let song;
+        switch(key) {
+            case "w":
+                song = new Audio("./sounds/" + songs[0]);
+
+                break;
+            case "a":
+                song = new Audio("./sounds/" + songs[1]);
+                break;
+            case "s":
+                song = new Audio("./sounds/" + songs[2]);
+                break;
+            case "d":
+                song = new Audio("./sounds/" + songs[3]);
+                break;
+            case "j":
+                song = new Audio("./sounds/" + songs[4]);
+                break;
+            case "k":
+                song = new Audio("./sounds/" + songs[5]);
+                break;
+            case "l":
+                song = new Audio("./sounds/" + songs[6]);
+                break;
+            default:
+                console.log("Invalid key pressed");
+
+        };
+        song.play();
+        
+};
+
+
